bd574229fe142568328edfdfc80bfa46be02539d
thijmendam <thijmenthijmen@live.nl>
2022-09-26 22:40:15 +0200

    keybindings (WIP)

--- src/components/KeyBinding/Bindings/Binding/Binding.tsx
+++ src/components/KeyBinding/Bindings/Binding/Binding.tsx
@@ -1,17 +1,20 @@
 import { AppSettings } from '../../../../../electron/types';
+import { modifiers, keys } from './keycodes';
 import { BindingStyle } from './styles';
 
 interface BindingProps {
   type: 'key' | 'modifier'
+  // eslint-disable-next-line react/no-unused-prop-types
   setting: keyof AppSettings['keyBindings']
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Binding(props: BindingProps): JSX.Element {
+  const keycodes = props.type === 'key' ? keys : modifiers;
+
   return (
     <BindingStyle>
       <select>
-        <option>{props.type}</option>
+        {keycodes.map((k) => <option value={k[1]}>{k[0]}</option>)}
       </select>
     </BindingStyle>
   );
--- src/components/KeyBinding/Bindings/Binding/styles.ts
+++ src/components/KeyBinding/Bindings/Binding/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+// TODO: actual styling
 export const BindingStyle = styled.div`
-
+  select {
+    max-width: 90px;
+  }
 `;
