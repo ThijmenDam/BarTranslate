b58073cece6373940d8ab2be45ca18b9bb8486d5
thijmendam <thijmenthijmen@live.nl>
2022-09-27 15:47:46 +0200

    keybindings (WIP)

--- src/components/KeyBinding/Bindings/Binding/Binding.tsx
+++ src/components/KeyBinding/Bindings/Binding/Binding.tsx
@@ -1,20 +1,27 @@
-import { AppSettings } from '../../../../../electron/types';
+import { FormEvent } from 'react';
+import { AppSettings, Modifier, Key } from '../../../../../electron/types';
 import { modifiers, keys } from './keycodes';
 import { BindingStyle } from './styles';
 
 interface BindingProps {
   type: 'key' | 'modifier'
-  // eslint-disable-next-line react/no-unused-prop-types
   setting: keyof AppSettings['keyBindings']
+  initialValue: Modifier | Key
 }
 
 export default function Binding(props: BindingProps): JSX.Element {
   const keycodes = props.type === 'key' ? keys : modifiers;
 
+  function onInput(event: FormEvent<HTMLSelectElement>) {
+    // TODO: change setting here
+    const settingValue = (event.target as HTMLOptionElement).value;
+    console.log(props.setting, settingValue);
+  }
+
   return (
     <BindingStyle>
-      <select>
-        {keycodes.map((k) => <option value={k[1]}>{k[0]}</option>)}
+      <select value={props.initialValue} onInput={onInput}>
+        {keycodes.map((k) => <option key={k[1]} value={k[1]}>{k[0]}</option>)}
       </select>
     </BindingStyle>
   );
