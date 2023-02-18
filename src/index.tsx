986718d98d5a31ade360b5b9dae6af598351ad7b
thijmendam <thijmenthijmen@live.nl>
2023-02-18 14:17:02 +0100

    Better initialization of React

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
